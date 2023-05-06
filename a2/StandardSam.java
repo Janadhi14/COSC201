6171eb8bb7339b0ef00227dc44c74c28150ba490
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-06 19:43:58 +1200

    constructor for StandardSam done

--- a2/StandardSam.java
+++ a2/StandardSam.java
@@ -1,21 +1,14 @@
 package cosc201.a2;
 
 import java.util.Set;
-
 import cosc201.a2.Potionmaster;
 
-
-public class StandardSam extends Potionmaster {
-   // method that will brew poritons 
-
-
+public class StandardSam extends Potionmaster{
+// constructor and using super becase we are in the subclass of potionMaster which already has potionarium in its constructor 
    public StandardSam(Potionarium potionarium){
-    super();
-
+      super(potionarium);
    }
-   public static void main(String[] args){
-    System.out.println(
 
-    );
-   }  
+   
+   
 }
