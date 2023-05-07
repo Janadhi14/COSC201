648e83b3b04e1c1c68d0faaa95b3ad819e8b51df
disja508 <100277240+Janadhi14@users.noreply.github.com>
2023-05-07 20:51:11 +1200

    added

--- a2/testClass.java
+++ a2/testClass.java
@@ -5,7 +5,11 @@ import java.util.*;
 public class testClass{
 
     public static void main(String[] args){
-        // need to call 
+        // need to call potioinarium
+        Potionarium potionarium = new Potionarium();
+        
+        Set<String> ingredients1 = new HashSet<>(Arrays.asList("ingredient1", "ingredient2", "ingredient3"));
+        System.out.println("Fill drawer 1: " + potionarium.fillDrawer(1, ingredients1)); // Expected output: true
         
     }
 }
