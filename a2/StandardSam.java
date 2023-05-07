a436269dcfeab940bbf5978908782955e9e41afc
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-07 19:52:24 +1200

    removed snapchat replies

--- a2/StandardSam.java
+++ a2/StandardSam.java
@@ -19,12 +19,3 @@ to brew a potion so we need to implement this in java through*/
 
 
 
-Ahahahah it's the thought that counts I guess 🥲
-
-its the litttteelll and random things i be romembering tho which is super odd dont you rekcon?
-
-
-neither!!! ive always just called them jam buns, but there are ones that dont have jam in the middle and i used to get super dissapoinated when ammala bought them 
-
-
-you mean scared alot** but i guess you get the full expxerience, even in leik sad movies if you cry youre emmersed in teh movie
