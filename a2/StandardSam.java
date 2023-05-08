c6d5b01d3fd52fb947fc1e0bf531b22318daf579
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-08 15:19:48 +1200

    standard sam jjj

--- a2/StandardSam.java
+++ a2/StandardSam.java
@@ -13,7 +13,19 @@ public class StandardSam extends Potionmaster{
 public String collectIngredients(List<String> ingredients){
    /*Sam starts with a potion recipe: a list (without duplicates) of ingredients that are needed
 to brew a potion so we need to implement this in java through*/
+   List<String> ingredientsMissing = new ArrayList<>();
+   Map<Long, List<String>> usedDrawers = new HashMap<>();
    // need to verify that we are able to get these ingredients from the drawers 
+   // for loop and check each ingredient is there
+   for(String ingredient: ingredients){ 
+      Set<Long> drawers = potionarium.getDrawers(ingredient); // going through and getting the ingredients 
+      // need to also check if the drawers are empty 
+      if(drawers.isEmpty()){ // if true that is going to eman that the ingredinet sint present 
+         ingredientsMissing.add(ingredient);
+ 
+      }
+
+   }
    
 }
    
