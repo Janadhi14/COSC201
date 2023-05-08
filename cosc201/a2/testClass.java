b2771e75d7a8350eb602b5f8583374ac8c4b549b
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-08 20:19:26 +1200

    moved files to cosc201 folder

---
+++ cosc201/a2/testClass.java
@@ -0,0 +1,22 @@
+package cosc201.a2;
+
+
+import java.util.*;
+
+public class testClass{
+
+    public static void main(String[] args){
+        // need to call potioinarium
+        Potionarium j = new Potionarium();
+        
+        Set<String> ingredientsList = new HashSet<>(Arrays.asList("ingredient1", "ingredient2", "ingredient3"));
+        System.out.println("Fill drawer 1: " + j.addIngredient(1, "a")); // Expected output: true
+        System.out.println("Fill drawer 2: " + j.addIngredient(2, "b")); // Expected output: true
+        System.out.println("Fill drawer 3: " + j.addIngredient(44, "c")); // Expected output: true
+        System.out.println("Fill drawer 4: " + j.fillDrawer(4, ingredientsList)); // Expected output: true
+        //System.out.println(potionarium.removeIngredient(1, "a")); // Expected output: false beacsue it is not in teh map
+        System.out.println("jjjjjj drawers: " + j.getInventory()); 
+        
+       
+    }
+    }
