acdb144d9ed79f80fa648aecfff304687a6c5a97
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-09 00:09:09 +1200

    test calss added loop

--- cosc201/a2/testClass.java
+++ cosc201/a2/testClass.java
@@ -1,22 +1,52 @@
 package cosc201.a2;
 
-
+import utilities.Timer;
 import java.util.*;
 
-public class testClass{
+public class testClass {
 
-    public static void main(String[] args){
-        // need to call potioinarium
-        Potionarium j = new Potionarium();
+    private static final Random RANDOM = new Random();
+    public static int numIngredients = 100;
+
+    public static void main(String[] args) {
+        Timer t = new Timer(); // creating a timer 
+        // Create a Potionarium object
+        Potionarium Cabinet = new Potionarium();
         
-        Set<String> ingredientsList = new HashSet<>(Arrays.asList("ingredient1", "ingredient2", "ingredient3"));
-        System.out.println("Fill drawer 1: " + j.addIngredient(1, "a")); // Expected output: true
-        System.out.println("Fill drawer 2: " + j.addIngredient(2, "b")); // Expected output: true
-        System.out.println("Fill drawer 3: " + j.addIngredient(44, "c")); // Expected output: true
-        System.out.println("Fill drawer 4: " + j.fillDrawer(4, ingredientsList)); // Expected output: true
-        //System.out.println(potionarium.removeIngredient(1, "a")); // Expected output: false beacsue it is not in teh map
-        System.out.println("jjjjjj drawers: " + j.getInventory()); 
-        
-       
+        // Add 100 random ingredients to the Potionarium for testing
+        for (int i = 0; i < numIngredients; i++) {
+            Set<String> ingredientsList = new HashSet<>(Collections.singletonList(generateRandomIngredient()));
+            Cabinet.fillDrawer(RANDOM.nextInt(1000), ingredientsList);
+        }
+
+        System.out.println("Cabinet Inventory:");
+        System.out.println(Cabinet.getInventory());
+
+        // Create a list of 10 ingredients that are all present in the Potionarium
+        List<String> ingredientsToCollect = new ArrayList<>(Cabinet.getInventory());
+        Collections.shuffle(ingredientsToCollect);
+        ingredientsToCollect = ingredientsToCollect.subList(0, 10);
+
+        System.out.println("Ingredients to Collect:");
+        System.out.println(ingredientsToCollect);
+
+        // Create a StandardSam object
+        StandardSam standardSam = new StandardSam(Cabinet);
+
+        // Test the collectIngredients method with 10 ingredients that are all present
+        String result1 = standardSam.collectIngredients(ingredientsToCollect);
+        System.out.println("Case 1: All ingredients are available\n" + result1);
     }
+
+    private static String generateRandomIngredient() {
+        int length = 5 + RANDOM.nextInt(6);
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            char c = (char) ('a' + RANDOM.nextInt(26));
+            sb.append(c);
+        }
+
+        return sb.toString();
     }
+}
