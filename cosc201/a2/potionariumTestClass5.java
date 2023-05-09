fa0cfab0517fa1afd949cd9d6385125bc9af6166
Janadhi Dissanayake <janadhi14@gmail.com>
2023-05-10 02:01:48 +1200

    test classes

--- cosc201/a2/potionariumTestClass5.java
+++ cosc201/a2/potionariumTestClass5.java
@@ -1,39 +1,44 @@
 package cosc201.a2;
 
-import java.util.*; 
+import java.util.Set;
+import java.util.HashSet;
 
 public class potionariumTestClass5 {
-    public static double milli = 1000000;
-    public static void main(String[] args) {
-        Potionarium potionarium;
-        Timer t = new Timer();
-        int[] sizes = {0, 20000, 40000, 60000, 80000, 100000, 120000, 140000, 160000, 180000, 200000};
-
-        int trials = 100;
-        int ingredientsPerDrawer = 1; // Adjust this number as needed
-
-        for (int size : sizes) {
-            long totalElapsedTime = 0;
-
-            for (int trial = 0; trial < trials; trial++) {
-                potionarium = new Potionarium();
-
-                t.start();
-
-                // Run the addIngredient method for each drawer
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < ingredientsPerDrawer; j++) {
-                        boolean added = potionarium.addIngredient(i, "ingredient" + j);
-                    }
-                }
-
-                t.stop();
-
-                totalElapsedTime += t.getTime();
-            }
-
-            long averageElapsedTime = totalElapsedTime / trials;
-            System.out.println(averageElapsedTime / milli);
+  
+  public static void main(String[] args) {
+    // Create a new Potionarium
+    Potionarium potionarium = new Potionarium();
+    
+    // Create a set of drawers
+    long[] drawers = {1, 2, 3, 4, 5};
+    
+    // Test with different numbers of ingredients
+    int[] numbersOfIngredients = {10, 100, 1000, 10000, 1000000};
+    
+    for (int n : numbersOfIngredients) {
+      // Create a set of ingredients
+      Set<String> ingredients = new HashSet<>();
+      for (int i = 0; i < n; i++) {
+        ingredients.add("Ingredient" + i);
+      }
+      
+      // Test adding ingredients to each drawer
+      for (long drawer : drawers) {
+        // Get start time
+        long startTime = System.currentTimeMillis();
+        
+        // Add ingredients
+        for (String ingredient : ingredients) {
+          potionarium.addIngredient(drawer, ingredient);
         }
+        
+        // Get end time
+        long endTime = System.currentTimeMillis();
+        
+        // Calculate and print elapsed time
+        long elapsedTime = endTime - startTime;
+        System.out.println("Time to add " + n + " ingredients to drawer " + drawer + ": " + elapsedTime + " ms");
+      }
     }
+  }
 }
--- cosc201/a2/potionariumTestClass7.java
+++ cosc201/a2/potionariumTestClass7.java
@@ -2,12 +2,13 @@ package cosc201.a2;
 
 import java.util.*;
 
-public class potionariumTestClass6 {
+public class potionariumTestClass7 {
     public static double milli = 1000000;
     public static void main(String[] args) {
         Potionarium potionarium;
         Timer t = new Timer();
-        int[] sizes = {1, 20000, 40000, 60000, 80000, 100000, 120000, 140000, 160000, 180000, 200000};
+        int[] sizes = { 200, 400, 600, 800, 1000, 1200, 1400, 1600, 1800, 2000
+        };
 
         int trials = 100;
 
@@ -21,33 +22,36 @@ public class potionariumTestClass6 {
                 // Populate the Potionarium with the current size of ingredients
                 for (int i = 0; i < size; i++) {
                     Set<String> ingredients = new HashSet<>();
-                    t.start();
-                    ingredients.add("ingredient" + i);
-                    t.stop();
-                    totalElapsedTime += t.getTime();
+                    for (int j = 0; j < size; j++) {
+                        ingredients.add("ingredient" + j);
+                    }
                     potionarium.fillDrawer(i, ingredients);
-                    
                 }
         
                 // Repeat the operation many times and measure the total time
                 for (int i = 0; i < size; i++) {
-                    // Record the start time
-                    
+                    Set<String> ingredientsToRemove = new HashSet<>();
+                    for (int j = 0; j < size; j++) {
+                        ingredientsToRemove.add("ingredient" + j);
+                    }
         
-                    // Run the removeIngredient method
-                    potionarium.removeIngredient(i, "ingredient" + i);
+                    // Record the start time
+                    t.start();
+        
+                    // Run the removeIngredients method
+                    boolean result = potionarium.removeIngredients(i, ingredientsToRemove);
         
                     // Record the end time
-                    
+                    t.stop();
         
                     // Accumulate the elapsed time
-                    
+                    totalElapsedTime += t.getTime();
                 }
             }
         
             // Calculate and print the average elapsed time
             long averageElapsedTime = totalElapsedTime / (trials * size);
-            System.out.println( size  + ", " + averageElapsedTime/milli);
+            System.out.println( size + ", " +averageElapsedTime/milli);
         }
     }
 }
